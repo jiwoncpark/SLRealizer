53cbfc5e094cf59c64de9453e758d885c477a78a
yeonjaek@stanford.edu <yeonjaek@stanford.edu>
2017-03-01 03:00:55 -0800

    updated basic coloring

--- doc/tutorials/plotting.py
+++ doc/tutorials/plotting.py
@@ -4,32 +4,32 @@ import math
 import matplotlib.pyplot as plt
 
 def plot_lens(lens, obsHist, convolve=False):
-	fig = plt.figure()
-	scale_factor = 2
-	print obsHist
-	sourceX = lens['XSRC'][0]
-	sourceY = lens['YSRC'][0]
-	source = plt.Circle((sourceX, sourceY), radius=obsHist[2]/scale_factor, alpha=0.1, fc='r')
-	lensX = lens['XIMG'][0]
-	lensY = lens['YIMG'][0]
-	plotX = 0
-	plotY = 0
-	# REALLY BAD STYLE JENNY YOU WOULD HAVE GOTTEN C FOR THIS
-	print abs(lensX)
-	print abs(sourceX)
-	if (abs(lensX[0]))>(abs(sourceX)):
-		plotX = abs(lensX[0])
-	else:
-		plotX = abs(sourceX)
-	if (abs(lensY[0]))>(abs(sourceY)):
-		plotY = abs(lensY[0])
-	else:
-		plotY = abs(sourceY)
-	seeing = plt.Circle((-plotY-obsHist[2]/scale_factor, -plotX-obsHist[2]/scale_factor), radius=obsHist[2]/scale_factor, alpha=0.1, fc='g')	
-	plt.ylim(-plotY-2*obsHist[2]/scale_factor, plotY+2*obsHist[2]/scale_factor)
-	plt.xlim(-plotX-2*obsHist[2]/scale_factor, plotX+2*obsHist[2]/scale_factor)
-	lens = plt.Circle((lensX[0], lensY[0]), radius=obsHist[2]/scale_factor, alpha=0.1, fc='b')	
-	plt.gca().add_patch(seeing)
-	plt.gca().add_patch(lens)
-	plt.gca().add_patch(source)
-
+    scale_factor = 2
+    sourceX = lens['XSRC'][0]
+    sourceY = lens['YSRC'][0]
+    lensX = lens['XIMG'][0]
+    lensY = lens['YIMG'][0]
+    if (abs(lensX[0]+sourceX))>(abs(sourceX)):
+        plotX = abs(lensX[0]+sourceX)
+    else:
+        plotX = abs(sourceX)
+    if (abs(lensY[0]+sourceY))>(abs(sourceY)):
+        plotY = abs(lensY[0]+sourceY)
+    else:
+        plotY = abs(sourceY)
+    fig = plt.figure(figsize=(4, 16))
+    plt.ylim(-plotY-2*obsHist[2]/scale_factor, plotY+2*obsHist[2]/scale_factor)
+    plt.xlim(-plotX-2*obsHist[2]/scale_factor, plotX+2*obsHist[2]/scale_factor)
+    if(True):
+        for i in range(4):
+            plotX = 0
+            plotY = 0
+            plotNumStr = "4"+"1"+str(i+1)
+            plotNum = int(plotNumStr)
+            sub = fig.add_subplot(plotNum)
+            source = plt.Circle((sourceX, sourceY), radius=obsHist[2]/scale_factor, alpha=0.1, fc='r')
+            lens = plt.Circle((lensX[i]+sourceX, lensY[i]+sourceY), radius=obsHist[2]/scale_factor, alpha=0.1, fc='b')
+            fig.gca().add_patch(lens)
+            fig.gca().add_patch(source)
+            seeing = plt.Circle((-plotY-obsHist[2]/scale_factor, -plotX-obsHist[2]/scale_factor), radius=obsHist[2]/scale_factor, alpha=0.1, fc='g')
+            plt.gca().add_patch(seeing)
