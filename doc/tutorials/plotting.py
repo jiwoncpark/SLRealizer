81a904bac66e578b00af6d73aca79d34386413b8
yeonjaek@stanford.edu <yeonjaek@stanford.edu>
2017-03-01 21:03:38 -0800

    plot starts to seem reasonablegit add *git add *

--- doc/tutorials/plotting.py
+++ doc/tutorials/plotting.py
@@ -4,6 +4,9 @@ import math
 import matplotlib.pyplot as plt
 
 def plot_lens(lens, obsHist, convolve=False):
+    #obsHist has MJD Filter FWHM 5sigmag
+    filterQuasar = obsHist[1] + '_SDSS_quasar'
+    filterLens = obsHist[1] + '_SDSS_Lens'
     scale_factor = 2
     sourceX = lens['XSRC'][0]
     sourceY = lens['YSRC'][0]
@@ -18,18 +21,19 @@ def plot_lens(lens, obsHist, convolve=False):
     else:
         plotY = abs(sourceY)
     fig = plt.figure(figsize=(4, 16))
-    plt.ylim(-plotY-2*obsHist[2]/scale_factor, plotY+2*obsHist[2]/scale_factor)
-    plt.xlim(-plotX-2*obsHist[2]/scale_factor, plotX+2*obsHist[2]/scale_factor)
+
     if(True):
         for i in range(4):
-            plotX = 0
-            plotY = 0
             plotNumStr = "4"+"1"+str(i+1)
             plotNum = int(plotNumStr)
             sub = fig.add_subplot(plotNum)
+            sub.set_ylim(-plotY-2*obsHist[2]/scale_factor, plotY+2*obsHist[2]/scale_factor)
+            sub.set_xlim(-plotX-2*obsHist[2]/scale_factor, plotX+2*obsHist[2]/scale_factor)
             source = plt.Circle((sourceX, sourceY), radius=obsHist[2]/scale_factor, alpha=0.1, fc='r')
             lens = plt.Circle((lensX[i]+sourceX, lensY[i]+sourceY), radius=obsHist[2]/scale_factor, alpha=0.1, fc='b')
             fig.gca().add_patch(lens)
             fig.gca().add_patch(source)
-            seeing = plt.Circle((-plotY-obsHist[2]/scale_factor, -plotX-obsHist[2]/scale_factor), radius=obsHist[2]/scale_factor, alpha=0.1, fc='g')
+            seeing = plt.Circle((-plotY+(obsHist[2]*0.7)/scale_factor, -plotX-(obsHist[2]*0.8)/scale_factor), radius=obsHist[2]/scale_factor, alpha=0.1, fc='g')
+            sub.set_title('Observation ' + str(i+1) + ' with ' + 'filter')
+            sub.legend((source, seeing, lens),('source', 'seeing', 'lens'))
             plt.gca().add_patch(seeing)
