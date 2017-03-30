93f7997daa5421060bfe5a2406a574dff0ebb1b5
yeonjaek@stanford.edu <yeonjaek@stanford.edu>
2017-03-30 16:42:59 +0900

    started calculating moments

--- python/test/slrealizer/SLRealizer.py
+++ python/test/slrealizer/SLRealizer.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pylab
 import matplotlib
 import math
+import moment
 
 class SLRealizer(object):
     
@@ -22,6 +23,9 @@ class SLRealizer(object):
     
     def plot_lens_random_date(self, lensID = 7176527, convolve=False):
         plotting.plot_lens_random_date(self, lensID, convolve)
+
+    def plot_zeroth_moment(self, lensID = 7176527):
+        moment.zeroMoment(self, lensID)
 
 """
     def determineAlpha(self, mag_ratio):
---
+++ python/test/slrealizer/moment.py
@@ -0,0 +1,28 @@
+import plotting
+import pandas as pd
+import matplotlib.pyplot as plt
+import pylab
+import matplotlib
+import math
+
+# weighted by the magnitude(or interchangably, flux)
+
+# Calculate magnitude
+def zeroMoment(self, lensID):
+    self.lens.get_lens(lensID)
+    # for loop
+    filterQuasar = currObs[1] + '_SDSS_quasar'
+    filterLens = currObs[1] + '_SDSS_lens'
+    lens_mag = currLens[filterLens]
+    quasar_mag = currLens[filterQuasar]
+
+def zeroMomentPlot
+
+# Calculate position
+def firstMoment
+
+def firstMomentPlot
+
+def secondMoment
+
+def secondMomentPlot
