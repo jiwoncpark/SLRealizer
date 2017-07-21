994398d934d7290d11c04219acaa5a120d0a23fc
yeonjaek@stanford.edu <yeonjaek@stanford.edu>
2017-07-21 14:59:49 -0700

    bug in moment fixed

--- python/desc/slrealizer/moment.py
+++ python/desc/slrealizer/moment.py
@@ -2,13 +2,15 @@ from __future__ import print_function
 import math
 import numpy as np
 
+magnitude_zeropoint = 10 # where we define the flux to be 1
+
 def calculate_lens_zeroth_moment(currObs, currLens):
     """
        Given a lensed system, calculate the zeroth moment(total flux) of lensing galaxy.
     """
     filterLens = currObs[1] + '_SDSS_lens'
     lens_mag = currLens[filterLens]
-    lensFlux = pow(2.5, lens_mag)
+    lensFlux = pow(2.5, magnitude_zeropoint-lens_mag)
     return lensFlux
 
 def calculate_image_zeroth_moment(currObs, currLens):
@@ -16,28 +18,11 @@ def calculate_image_zeroth_moment(currObs, currLens):
         Given a lensed system, calculate the zeroth moment(total flux) of quasar's images.
     """
     # Initialize the flux
-    filterQuasar = currObs[1] + '_SDSS_quasar'
-    imageFlux = 0
-    nImage = 0
-    if int(currLens['NIMG'][0]) is 4:
-        #print('here')
-        nImage = 4
-    if int(currLens['NIMG'][0]) is 2:
-        #print('here')
-        nImage = 2
-    #print(nImage)
-    for i in range(nImage):
-        if int(currLens['MAG'][0][i]) is 0:
-            imageMag = currLens[filterQuasar]
-        else :
-            #print(currLens[filterQuasar])
-            #print(abs(currLens['MAG'][0][i]))
-            imageMag= currLens[filterQuasar] - 2.5*np.log10(abs(currLens['MAG'][0][i]))
-        #print('imageMag')
-        #print(imageMag)
-        imageFlux += pow(2.5, -imageMag)
-        #print('imageFlux')
-        #print(imageFlux)
+    imageFlux=0
+    for i in range(int(currLens['NIMG'][0])):
+        print(currLens['MAG'])
+        imageMag = currLens['MAG'][0][i]
+        imageFlux += pow(2.5, magnitude_zeropoint-imageMag)
     return imageFlux
 
 def calculate_image_first_moment(currObs, currLens):
@@ -47,18 +32,46 @@ def calculate_image_first_moment(currObs, currLens):
     imageXMoment = 0
     imageYMoment = 0
     imageFlux = 0
-    filterQuasar = currObs[1] + '_SDSS_quasar'
-    for i in range(4):
-        if int(currLens['MAG'][0][i]) is 0:
-            imageMag = currLens[filterQuasar]
-        else:
-            imageMag= currLens[filterQuasar] - 2.5*np.log10(abs(currLens['MAG'][0][i]))
-        imageFlux += pow(2.5, -imageMag)
+    #filterQuasar = currObs[1] + '_SDSS_quasar'
+    for i in xrange(currLens['NIMG']):
+        #if int(currLens['MAG'][0][i]) is 0:
+        #    imageMag = currLens[filterQuasar]
+        #else:
+        imageMag= currLens['MAG'][0][i]
+        imageFlux += pow(2.5, magnitude_zeropoint-imageMag)
         # position * flux = moment
         imageCurrXMoment = currLens['XIMG'][0][i] * imageFlux
         imageCurrYMoment = currLens['YIMG'][0][i] * imageFlux
         imageXMoment += imageCurrXMoment
         imageYMoment += imageCurrYMoment
+    return imageXMoment, imageYMoment
+
+def calculate_image_second_moment(currObs, currLens):
+    """                                                                                                          
+        Given a lensed system, calculate the first moment(total moment for x and y) of quasar's images.          
+    """
+    imageXMoment = 0
+    imageYMoment = 0
+    imageFlux = 0
+    for i in xrange(currLens['NIMG']):
+        imageMag= currLens['MAG'][0][i]
+        imageFlux += pow(2.5, magnitude_zeropoint-imageMag)
+        imageCurrXMoment = currLens['XIMG'][0][i] * currLens['XIMG'][0][i] * imageFlux
+        imageCurrYMoment = currLens['YIMG'][0][i] * currLens['YIMG'][0][i] * imageFlux
+        imageXMoment += imageCurrXMoment
+        imageYMoment += imageCurrYMoment
+    return imageXMoment, imageYMoment
+
+def calculate_image_second_moment_nc(currObs, currLens):
+    """
+        Given a lensed system, calculate the first moment(total moment for x and y) of quasar's images.
+        """
+    imageXMoment = 0
+    imageYMoment = 0
+    PSF_HWHM = currObs[2]
+    for i in xrange(currLens['NIMG']):
+        imageXMoment += currLens['XIMG'][0][i] * currLens['XIMG'][0][i] * PSF_HWHM
+        imageYMoment += currLens['YIMG'][0][i] * currLens['YIMG'][0][i] * PSF_HWHM
     return imageXMoment, imageYMoment
 
 def zeroth_moment(currObs, currLens):
@@ -85,11 +98,6 @@ def first_moment(currObs, currLens):
     yMoment += calculate_image_first_moment(currObs, currLens)[1]
     xMoment += calculate_image_first_moment(currObs, currLens)[0]
 
-    #print('Image X Moment')
-    #print(xMoment)
-    #print('Image Y Moment')
-    #print(yMoment)
-
     return xMoment, yMoment
 
 
@@ -98,29 +106,31 @@ def second_moment(currObs, currLens, convolved=False):
         Given a lens system, returns the second moment for each x and y axis
     """
     
-    xMoment, yMoment = first_moment(currObs, currLens)
     
     
-    xSecondMoment = xMoment * xMoment
-    ySecondMoment = yMoment * yMoment
-    
-    yTotalSigma = 0
-    xTotalSigma = 0
-    if currLens['NIMG'][0] is 2:
-        yTotalSigma += currObs[2] * 2
-        xTotalSigma += currObs[2] * 2
-    if currLens ['NIMG'][0] is 4:
-        yTotalSigma += currObs[2] * 4
-        xTotalSigma += currObs[2] * 4
-    if not convolved:
-        yTotalSigma += currObs[2]
-        xTotalSigma += currObs[2]
-    else:
-        lens_FWHM = currLens['REFF']
-        galaxy_HWHM = desc.slrealizer.convolve(PSF_HWHM, lens_FWHM)/scale_factor
-        yTotalSigma += galaxy_HWHM
-        xTotalSigma += galaxy_HWHM
+    filterLens = currObs[1] + '_SDSS_lens'
+    filterQuasar = currObs[1] + '_SDSS_quasar'
+    # Initialize the moment                                                                                      
+    yMoment = 0
+    xMoment = 0
 
-    xSecondMoment += xTotalSigma * xTotalSigma
-    ySecondMoment += yTotalSigma * yTotalSigma
-    return xSecondMoment, ySecondMoment
+    """
+    # First approach, using the real definition of the second moment
+    # We calculate the contributions to the first moment by the lensing galaxy, and add it to each x and y momen\ts.
+    xMoment += calculate_lens_zeroth_moment(currObs, currLens) * currLens['XSRC'][0] * currLens['XSRC'][0]
+    yMoment += calculate_lens_zeroth_moment(currObs, currLens) * currLens['YSRC'][0] * currLens['YSRC'][0]
+    # Add moments calculated through `calculate_image_first_moment` method                                       
+    yMoment += calculate_image_second_moment(currObs, currLens)[1]
+    xMoment += calculate_image_second_moment(currObs, currLens)[0]
+    """
+    
+    # Second approach, non-convolved galaxy with quasars using FWHM
+    PSF_HWHM = currObs[2]
+    xMoment += PSF_HWHM * currLens['XSRC'][0] * currLens['XSRC'][0]
+    yMoment += PSF_HWHM * currLens['YSRC'][0] * currLens['YSRC'][0]
+    yMoment += calculate_image_second_moment_nc(currObs, currLens)[1]
+    xMoment += calculate_image_second_moment_nc(currObs, currLens)[0]
+    
+    return xMoment, yMoment    
+
+#return xSecondMoment, ySecondMoment
