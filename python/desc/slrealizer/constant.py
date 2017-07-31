67c00352507ff0d8077768b6b0b2b15a5bd02b34
yeonjaek@stanford.edu <yeonjaek@stanford.edu>
2017-07-31 12:00:17 -0700

    deleted irrelevant files

--- python/desc/slrealizer/constant.py
+++ python/desc/slrealizer/constant.py
@@ -4,7 +4,13 @@ All the constants should be accessed through the methods here, so that they woul
 from astropy.coordinates import SkyCoord
 
 def return_zeropoint():
+    """
+    returns zeropoint for the SDSS magnitude system
+    """
     return 23.5
 
 def return_obs_RA_DEC():
+    """
+    Return OM10's lensed system's position
+    """
     return SkyCoord('03h 32m 30s', '10d 00m 24s')
