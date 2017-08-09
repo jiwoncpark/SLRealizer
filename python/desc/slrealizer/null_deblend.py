1dbe01d87a77767fdb594042ce6bc25d5eaac51c
yeonjaek@stanford.edu <yeonjaek@stanford.edu>
2017-08-09 16:34:12 -0700

    changing from constant declaration to getter

--- python/desc/slrealizer/null_deblend.py
+++ python/desc/slrealizer/null_deblend.py
@@ -24,18 +24,6 @@ import skimage
 import skimage.measure
 from skimage.measure import moments
 
-# ======================================================================                           
-
-#global variable that controls the size of the plot
-x_min = -5.0
-x_max = 5.0
-y_min = -5.0
-y_max = 5.0
-distance = 0.01
-
-number_of_rows = int((x_max - x_min)/distance)
-number_of_columns = int((y_max - y_min)/distance)
-#=======================================================================
 
 """
 The methods in this file will be called by `deblend.py`. 
@@ -48,6 +36,8 @@ def null_deblend(image2):
     Given a 2d input array, this method generates an output as null deblender would do.
     Returns the array's zeroth moment(flux), first moments, and second moments
     """
+    x_min, x_max, y_min, y_max, distance = desc.slrealizer.get_x_min(), desc.slrealizer.get_x_max(), desc.slrealizer.get_y_min(), desc.slrealizer.get_y_max(), desc.slrealizer.get_distance()
+    number_of_rows, number_of_columns = int((x_max - x_min)/distance), int((y_max - y_min)/distance)
     #skimage.measure.moments return four by four matrix, whose entrees are x_column, y_row's moments. i.e) matrix[0][1] is the first moment of x (which is a little counter intuitive)
     moment_matrix = skimage.measure.moments(image2)
     zeroth_moment = moment_matrix[0][0]
@@ -67,6 +57,8 @@ def null_deblend_plot(flux, first_moment_x, first_moment_y, covariance_matrix):
     """
     Given the zeroth moment, first moment, and the second moment, this method draws the realistic color plot that represents the shape of null-deblended object.
     """
+    x_min, x_max, y_min, y_max, distance = desc.slrealizer.get_x_min(), desc.slrealizer.get_x_max(), desc.slrealizer.get_y_min(), desc.slrealizer.get_y_max(), desc.slrealizer.get_distance()
+    number_of_rows, number_of_columns = int((x_max - x_min)/distance), int((y_max - y_min)/distance)
     x, y = np.mgrid[x_min:x_max:distance, y_min:y_max:distance]
     pos = np.dstack((x, y))
     rv = scipy.stats.multivariate_normal([first_moment_x,first_moment_y], covariance_matrix, allow_singular=True) #FIX BUG     
