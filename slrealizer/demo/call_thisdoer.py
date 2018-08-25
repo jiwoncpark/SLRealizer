121d3016d4a7430b04fd385df307be48633d4395
jiwoncpark <jiwon.christine.park@gmail.com>
2018-08-25 00:34:41 -0700

    Import specified package name first

--- slrealizer/demo/call_thisdoer.py
+++ slrealizer/demo/call_thisdoer.py
@@ -1,12 +1,13 @@
 from __future__ import absolute_import
 
 # Assumes sl-realizer is package root
-from slrealizer.do_this import ThisDoer
+import slrealizer
+#from slrealizer.do_this import ThisDoer
 # Assumes slrealizer is package root
 #from do_this import ThisDoer
 
 import os.path
-# Assumes sl-realizer is package root
+ Assumes sl-realizer is package root
 print('SLRealizer/slrealizer/data/data.py', os.path.exists('SLRealizer/slrealizer/data/data.py'))
 # Assumes slrealizer is package root
 print('data/data.py', os.path.exists('data/data.py'))
