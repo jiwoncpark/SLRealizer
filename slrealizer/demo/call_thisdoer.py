e6dc35e5f52311eb8864ac61d036cfa03b5dad9e
jiwoncpark <jiwon.christine.park@gmail.com>
2018-08-25 00:04:43 -0700

    Include package name in import statement

--- slrealizer/demo/call_thisdoer.py
+++ slrealizer/demo/call_thisdoer.py
@@ -1,12 +1,12 @@
 from __future__ import absolute_import
 
 # Assumes sl-realizer is package root
-#from slrealizer.do_this import ThisDoer
+from SLRealizer.slrealizer.do_this import ThisDoer
 # Assumes slrealizer is package root
-from do_this import ThisDoer
+#from do_this import ThisDoer
 
 import os.path
 # Assumes sl-realizer is package root
-print('slrealizer/data/data.py', os.path.exists('slrealizer/data/data.py'))
+print('SLRealizer/slrealizer/data/data.py', os.path.exists('SLRealizer/slrealizer/data/data.py'))
 # Assumes slrealizer is package root
 print('data/data.py', os.path.exists('data/data.py'))
