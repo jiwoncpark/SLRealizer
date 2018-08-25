8f8a53bd2f894589af8b46a8446cefce551e8a9e
jiwoncpark <jiwon.christine.park@gmail.com>
2018-08-25 00:39:25 -0700

    Call class function

--- slrealizer/demo/call_thisdoer.py
+++ slrealizer/demo/call_thisdoer.py
@@ -5,9 +5,11 @@ import slrealizer
 #from slrealizer.do_this import ThisDoer
 # Assumes slrealizer is package root
 #from do_this import ThisDoer
+yo = slrealizer.do_this.ThisDoer(3)
+yo.get_this()
 
 import os.path
- Assumes sl-realizer is package root
+# Assumes sl-realizer is package root
 print('SLRealizer/slrealizer/data/data.py', os.path.exists('SLRealizer/slrealizer/data/data.py'))
 # Assumes slrealizer is package root
 print('data/data.py', os.path.exists('data/data.py'))
